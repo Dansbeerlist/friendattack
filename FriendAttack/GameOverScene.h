74193ac2e7e408afe0cba544a171638784e9fe53
Dan Warter <danielappinfo@gmail.com>
2014-04-26 18:19:27 -0700

    first commit

---
+++ FriendAttack/GameOverScene.h
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.h
+//  FriendAttack
+//
+//  Created by Dan Warter on 4/26/14.
+//  Copyright (c) 2014 Dan Warter. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverScene : SKScene
+
+@end
---
+++ FriendAttack/GameOverScene.m
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.m
+//  FriendAttack
+//
+//  Created by Dan Warter on 4/26/14.
+//  Copyright (c) 2014 Dan Warter. All rights reserved.
+//
+
+#import "GameOverScene.h"
+
+@implementation GameOverScene
+
+@end
